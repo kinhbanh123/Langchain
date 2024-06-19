d38597b6d155add6ab71b5e1dd41aa4bdb79dfd1
Trần Nguyên Bình <tnguyenbinhxyz@gmail.com>
2024-06-19 14:08:39 +0700

    Add context for chain

--- app/main.py
+++ app/main.py
@@ -2,10 +2,15 @@ import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from langchain import HuggingFaceHub, PromptTemplate, LLMChain
+from langchain.document_loaders import PyPDFLoader  
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma #vector store
+from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter  
 from dotenv import load_dotenv
+import re
 
-# Load environment variables
-load_dotenv()
+load_dotenv() #Load something secret
 huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 if not huggingfacehub_api_token:
     raise ValueError("HUGGINGFACEHUB_API_TOKEN is not set in the environment")
@@ -14,14 +19,20 @@ if not huggingfacehub_api_token:
 repo_id = "HuggingFaceH4/zephyr-7b-beta"
 llm = HuggingFaceHub(huggingfacehub_api_token=huggingfacehub_api_token,
                      repo_id=repo_id,
-                     model_kwargs={"temperature":0.7, "max_new_tokens":500})
+                     model_kwargs={"temperature":0.7, "max_new_tokens":1000})
 
 template = """Question: {question}
 Answer: Let's give a detailed answer."""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
 chain = LLMChain(prompt=prompt, llm=llm)
-
+loader = PyPDFLoader("./data/Hololive Production.pdf")
+pages = loader.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+docs = splitter.split_documents(pages)
+embeddings = HuggingFaceEmbeddings()
+doc_search = Chroma.from_documents(docs, embeddings)
+retrieval_chain = RetrievalQA.from_chain_type(llm, chain_type='stuff', retriever=doc_search.as_retriever())
 # Define FastAPI app
 app = FastAPI()
 
@@ -38,8 +49,13 @@ class ActionResponse(BaseModel):
 @app.post("/chat/", response_model=Answer)
 async def chat(question:str):
     try:
-        answer = chain.run(question)
-        return Answer(answer=answer)
+        answer = retrieval_chain.run(question)
+        pattern = r"Helpful Answer:\s*(.*)"
+        match = re.search(pattern, answer, re.DOTALL)
+        if match:
+            helpful_answer = match.group(1).strip()
+
+        return Answer(answer=helpful_answer)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 @app.get("/")
